3760e955c418f8ca79bcab27968854c5b45d1903
hugoc1944 <125464948+hugoc1944@users.noreply.github.com>
2024-05-06 18:04:28 +0100

    Local meditaions JSON file added

---
+++ App/components/parents/Category.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Category({data, view}) {
+    
+  return (
+    <View>
+      <Text>Category</Text>
+    </View>
+  )
+}
