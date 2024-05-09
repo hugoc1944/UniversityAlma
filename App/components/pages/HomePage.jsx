10137b90692b56fdbd07ee9a142e7faf6eff8d13
RicardoNuno <118680747+RicardoNuno@users.noreply.github.com>
2024-05-09 04:04:25 +0200

    Homepage
    Homepage Done

--- App/components/pages/HomePage.jsx
+++ App/components/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, Text } from 'react-native';
 import FourSquareButton from '../elements/CornerButton';
 import ProfilePicture from '../elements/ProfilePicture';
 import MeditationBox from '../elements/MeditationBox';
@@ -21,18 +21,22 @@ export default function HomePage() {
         <TopHeader data={dataExemplo}/>
         <CategoryNav onSelectCategory={handleCategorySelect} />
         <HighlightedSession/>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.horizontalScrollContainer}
-        >
-          {meditationData
-            .filter(course => !selectedCategory || course.category === selectedCategory)
-            .map((course, index) => (
-              <MeditationBox key={index} data={course} />
-            ))}
-        </ScrollView>
       </View>
+      <View style={styles.textCont}>
+        <Text style={styles.textL}>Explore Meditations</Text>
+        <Text style={styles.textR}>View All</Text>
+      </View>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.horizontalScrollContainer}
+      >
+        {meditationData
+          .filter(course => !selectedCategory || course.category === selectedCategory)
+          .map((course, index) => (
+            <MeditationBox key={index} data={course} />
+          ))}
+      </ScrollView>
     </ScrollView>
   );
 }
@@ -44,11 +48,25 @@ const styles = StyleSheet.create({
   },
   horizontalScrollContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
     padding: 15,
     gap: 10
   },
   vertScroll: {
     alignItems: 'center',
-  }
+  },
+  textCont: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textL: {
+    fontSize: 21,
+    paddingLeft: 20,
+    fontWeight: 'bold',
+    color: '#081E3F'
+  },
+  textR: {
+    fontSize: 15,
+    paddingRight: 20,
+    color: 'rgba(8, 30, 63, 0.5)'
+  },
 });
